69735195ec5327594e63cb21292153c75d235127
Biznaz <aldhelm7@gmail.com>
2021-07-30 18:03:28 -0700

    more changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 
 const app = express();
 const PORT = process.env.PORT || 5000;
